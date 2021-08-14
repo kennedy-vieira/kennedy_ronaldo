e8be66b660c48765aeec24bac62147b175107bc2
Kennedy Vieira <kennedycvieira@gmail.com>
2021-08-13 23:31:13 -0300

    login / seleção de usuario implementado

--- prototipo/agenda1/lib/interfaceLogin.dart
+++ prototipo/agenda1/lib/interfaceLogin.dart
@@ -5,52 +5,7 @@ import 'package:flutter/material.dart';
 import 'usuario.dart';
 import 'dataBase.dart';
 
-class InterfaceBemVindo extends StatefulWidget {
-  @override
-  _InterfaceBemVindoState createState() => new _InterfaceBemVindoState();
-}
-
-class _InterfaceBemVindoState extends State<InterfaceBemVindo> {
-  List<Usuario> listausuarios = [];
-  // lista que armazena os usuarios a serem exibidos
-
-  void carregausuarios() async {
-    var usu = await dbController().getUsuarios();
-    setState(() {
-      listausuarios = usu;
-    });
-  }
-
-  void initState() {
-    carregausuarios();
-  }
-
-  Future<dynamic> _showOptions(BuildContext context) async {
-    return showDialog<int>(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Selecione o usuario'),
-            content:
-                Text('tela de login deve aparecer a lista de usuarios aqui'),
-            actions: [
-              TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => MyHomePage(
-                                  title: "Pagina inicial",
-                                  idUsuario: 1,
-                                )));
-                  },
-                  child: Text('ok'))
-            ],
-          );
-        });
-  }
-
+class InterfaceBemVindo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,7 +18,10 @@ class _InterfaceBemVindoState extends State<InterfaceBemVindo> {
             children: [
               TextButton(
                   onPressed: () {
-                    _showOptions(context);
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => InterfaceSelecionaUsuario()));
                   },
                   child: Text('Login')),
               TextButton(
@@ -79,6 +37,74 @@ class _InterfaceBemVindoState extends State<InterfaceBemVindo> {
         ),
       ),
     );
+  }
+}
+
+class InterfaceSelecionaUsuario extends StatefulWidget {
+  @override
+  _InterfaceSelecionaUsuarioState createState() =>
+      new _InterfaceSelecionaUsuarioState();
+}
+
+class _InterfaceSelecionaUsuarioState extends State<InterfaceSelecionaUsuario> {
+  List<Usuario> listausuarios = [];
+  // lista que armazena os usuarios a serem exibidos
+
+  void carregausuarios() async {
+    var usu = await dbController().getUsuarios();
+    setState(() {
+      listausuarios = usu;
+    });
+  }
+
+  void initState() {
+    carregausuarios();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (listausuarios.length == 0) {
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('Login'),
+          ),
+          body: TextButton(
+            child: Text('Cadastre um usuario primeiro'),
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => InterfaceCadastroUsuario()));
+            },
+          ));
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Login'),
+        ),
+        body: Center(
+            child: ListView.builder(
+                itemCount: listausuarios.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Container(
+                      child: TextButton(
+                        child: Text(listausuarios[index].nome),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => MyHomePage(
+                                      title: 'Página Inicial',
+                                      idUsuario: listausuarios[index].id)));
+                        },
+                      ),
+                    ),
+                  );
+                })),
+      );
+    }
   }
 }
 
